8fe923a4daa7b856407414539ddb5ba569cf3993
junyoung1125 <wnsdud060451@gmail.com>
2025-04-19 23:06:41 +0900

    마이페이지파트 화면구현완료

--- lotteOn/src/main/java/kr/co/lotteOn/controller/MyPageController.java
+++ lotteOn/src/main/java/kr/co/lotteOn/controller/MyPageController.java
@@ -15,15 +15,36 @@ public class MyPageController {
     }
 
     //마이페이지 - 전체주문내역
-
+    @GetMapping("/my_order")
+    public String myOrder() {
+        return "/myPage/my_order";
+    }
     //마이페이지 - 포인트내역
+    @GetMapping("/my_point")
+    public String myPoint() {
+        return "/myPage/my_point";
+    }
 
     //마이페이지 - 쿠폰
+    @GetMapping("/my_coupon")
+    public String myCoupon() {
+        return "/myPage/my_coupon";
+    }
 
     //마이페이지 - 나의리뷰
+    @GetMapping("/my_review")
+    public String myReview() {
+        return "/myPage/my_review";
+    }
 
     //마이페이지 - 문의하기
-
+    @GetMapping("/my_qna")
+    public String myQna() {
+        return "/myPage/my_qna";
+    }
     //마이페이지 - 나의설정
-
+    @GetMapping("/my_info")
+    public String myInfo() {
+        return "/myPage/my_info";
+    }
 }
