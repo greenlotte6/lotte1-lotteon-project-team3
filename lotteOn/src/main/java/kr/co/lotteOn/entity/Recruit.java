f5afbf217cf617ed8e565abc5d01633da52d1bdd
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-04-24 17:12:36 +0900

    recruit 관리자 마무리

--- lotteOn/src/main/java/kr/co/lotteOn/entity/Recruit.java
+++ lotteOn/src/main/java/kr/co/lotteOn/entity/Recruit.java
@@ -6,7 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -25,17 +27,15 @@ public class Recruit {
     private String experience;  //경력
     private String employType;  //채용형태
     private String title;       //글제목
-    private String content;     //글내용
+    private String etc;     //글내용
     private String status;      //채용중, 채용마감
 
     @CreationTimestamp
     private LocalDateTime regDate;     //등록날짜
 
-    @CreationTimestamp
-    private LocalDateTime startDate;     //시작날짜
+    private String startDate;     //시작날짜
 
-    @CreationTimestamp
-    private LocalDateTime endDate;     //종료날짜
+    private String endDate;     //종료날짜
 
     //private String writer;      //작성자 = 관리자(Admin)
     //추가필드
