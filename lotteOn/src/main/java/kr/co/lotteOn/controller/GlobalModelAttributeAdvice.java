88bee5bb168121e307269a481c6b8bc070b34a6d
진우 <128763219+Jinwoooooooo@users.noreply.github.com>
2025-05-05 19:30:08 +0900

    관리자 기본설정 완료 상품 리스트 display: flex -> grid 변경 완료

--- lotteOn/src/main/java/kr/co/lotteOn/controller/GlobalModelAttributeAdvice.java
+++ lotteOn/src/main/java/kr/co/lotteOn/controller/GlobalModelAttributeAdvice.java
@@ -1,6 +1,8 @@
 package kr.co.lotteOn.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kr.co.lotteOn.entity.Config;
+import kr.co.lotteOn.service.ConfigService;
 import kr.co.lotteOn.service.VersionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,9 +15,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class GlobalModelAttributeAdvice {
 
     private final VersionService versionService;
+    private final ConfigService configService;
 
     @ModelAttribute("selectedVersionId")
     public String addLatestVersionIdToModel() {
         return versionService.getLatestVersionId();
+    }
+
+    @ModelAttribute("config")
+    public Config config() {
+        return configService.findById(1);
     }
 }
