2ca998b27eee28d4aea4adefd87d97a0017b22a8
junyoung1125 <wnsdud060451@gmail.com>
2025-05-12 17:09:23 +0900

    매출현황 목록조회 진행중

---
+++ lotteOn/src/main/java/kr/co/lotteOn/entity/Sales.java
@@ -0,0 +1,31 @@
+package kr.co.lotteOn.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Sales")
+public class Sales {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int salesNo;
+
+
+    @ManyToOne
+    @JoinColumn(name = "sellerId", referencedColumnName = "sellerId", insertable = false, updatable = false)
+    private Seller seller;
+
+
+}
