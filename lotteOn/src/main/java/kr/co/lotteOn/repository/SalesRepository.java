ebb046e289fe1c59519726ef8937f699c4f10a3d
junyoung1125 <wnsdud060451@gmail.com>
2025-05-13 17:10:31 +0900

    매출현황 진행중

--- lotteOn/src/main/java/kr/co/lotteOn/repository/SalesRepository.java
+++ lotteOn/src/main/java/kr/co/lotteOn/repository/SalesRepository.java
@@ -12,18 +12,5 @@ import java.util.List;
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, Integer> {
 
-    //    @Query("SELECT new kr.co.lotteOn.dto.SalesDTO(s.salesNo, s.seller.companyName, s.seller.businessNo) " +
-//            "FROM Sales s")
-    @Query("SELECT new kr.co.lotteOn.dto.SalesDTO(s.salesNo, s.seller.companyName, s.seller.businessNo) FROM Sales s LEFT JOIN s.seller")
-    List<SalesDTO> findAllSales();
-
-    @Query(value = "SELECT s.companyName, SUM(oi.quantity) AS orderCount " +
-            "FROM OrderItem oi " +
-            "JOIN Product p ON oi.productCode = p.productCode " +
-            "JOIN Seller s ON p.companyName = s.companyName " +
-            "GROUP BY s.companyName", nativeQuery = true)
-    List<Object[]> findOrderCountsBySeller();
-
-
 
 }
