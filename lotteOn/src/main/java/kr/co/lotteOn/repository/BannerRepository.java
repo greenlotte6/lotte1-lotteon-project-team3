097435868e040822be21f9d7950ae1a0c45c7e29
jinwoooooooo <jinwooya55@gmail.com>
2025-05-14 17:24:39 +0900

    여러가지 수정

--- lotteOn/src/main/java/kr/co/lotteOn/repository/BannerRepository.java
+++ lotteOn/src/main/java/kr/co/lotteOn/repository/BannerRepository.java
@@ -4,8 +4,10 @@ import kr.co.lotteOn.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BannerRepository extends JpaRepository<Banner, Integer> {
 
-    public Banner findByLocation(String location);
+    List<Banner> findByLocation(String location);
 }
