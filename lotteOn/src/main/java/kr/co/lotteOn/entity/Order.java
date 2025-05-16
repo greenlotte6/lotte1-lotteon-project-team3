17a0ba00055460c4e4e13f59f600207e45880e88
wnstj050505 <wnstj050505@gmail.com>
2025-05-16 11:42:47 +0900

    마이페이지 수정

--- lotteOn/src/main/java/kr/co/lotteOn/entity/Order.java
+++ lotteOn/src/main/java/kr/co/lotteOn/entity/Order.java
@@ -46,9 +46,6 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     @Builder.Default
     private List<OrderItem> items = new ArrayList<>();
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "productCode", referencedColumnName = "productCode")
-    private Product product;
+    
 }
 
